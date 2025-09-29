a0eb35b734ec09560b7f14f19b9149df7bdef7ec
Jiachen Liu <jiachen_liu2002@163.com>
2025-09-28 20:42:05 -0500

    Change default logging level in get_logger function from INFO to DEBUG

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import logging
 
-def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+def get_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
         logger.setLevel(level)
